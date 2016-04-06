943db2054b7f4192602f52f16199a69b3b30aa2e
andycheng <ccjeng@gmail.com>
2016-04-06 11:09:13 +0800

    no message

---
+++ app/src/main/java/com/ccjeng/stock/utils/Constant.java
@@ -0,0 +1,14 @@
+package com.ccjeng.stock.utils;
+
+/**
+ * Created by andycheng on 2016/3/13.
+ */
+public class Constant {
+
+    public final static String ENDPOINT_MOD = "http://dev.markitondemand.com/MODApis/Api/v2/";
+    public final static String ENDPOINT_YQL = "http://query.yahooapis.com/v1/public/";
+    public final static String ENDPOINT_YAHOO_CHART = "http://chartapi.finance.yahoo.com/instrument/1.0/";
+    public final static String ENDPOINT_GOOGLE = "http://www.google.com/finance/";
+    public final static String ENDPOINT_CUSTOM = "http://stockapi-ccjeng.rhcloud.com/";
+
+}
