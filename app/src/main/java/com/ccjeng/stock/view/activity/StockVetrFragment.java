5e7274ad5d2de785acc49b8761c1769321f258f9
andycheng <ccjeng@gmail.com>
2016-09-02 09:36:50 +0800

    Vetr Pie chart

--- app/src/main/java/com/ccjeng/stock/view/activity/StockVetrFragment.java
+++ app/src/main/java/com/ccjeng/stock/view/activity/StockVetrFragment.java
@@ -15,6 +15,12 @@ import com.ccjeng.stock.model.interfaces.ISecurityInfoCallback;
 import com.ccjeng.stock.model.vetr.SecurityInfo;
 import com.ccjeng.stock.view.adapter.VetrDetailsAdapter;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+
+import java.util.ArrayList;
 
 //import com.github.mikephil.charting.data.PieEntry;
 
@@ -33,7 +39,6 @@ public class StockVetrFragment {
     private RatingBar ratingBar;
     private ListView detailsColumn;
     private PieChart pieChart;
-    private PieChart pieChart2;
 
 
     public StockVetrFragment(View view, Context context, StockQuote currentStock) {
@@ -47,7 +52,6 @@ public class StockVetrFragment {
         ratingBar = (RatingBar)  view.findViewById(R.id.ratingBar);
         detailsColumn = (ListView) view.findViewById(R.id.vetrDetails);
         pieChart = (PieChart) view.findViewById(R.id.piechart);
-        pieChart2 = (PieChart) view.findViewById(R.id.piechart2);
 
 
     }
@@ -66,7 +70,7 @@ public class StockVetrFragment {
                 VetrDetailsAdapter adapter = new VetrDetailsAdapter(context, VetrAggratingcalcItem.Column(context, securityInfo.getData().getAggratingcalc()));
                 detailsColumn.setAdapter(adapter);
 
-                //setPieChart(securityInfo);
+                setPieChart(securityInfo);
                 //setStrongPieChart(securityInfo);
             }
         };
@@ -103,79 +107,34 @@ public class StockVetrFragment {
         pieChart.setHighlightPerTapEnabled(true);
 
         //set data
-        /*
-        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
-        entries.add(new PieEntry(securityInfo.getData().getAggratingcalc().getTotalSellRatings(), "Sell"));
-        entries.add(new PieEntry(securityInfo.getData().getAggratingcalc().getTotalHoldRatings(), "Hold"));
-        entries.add(new PieEntry(securityInfo.getData().getAggratingcalc().getTotalBuyRatings(), "Buy"));
-        PieDataSet dataSet = new PieDataSet(entries, "Crowd Rating");
+        ArrayList<Entry> yVals = new ArrayList<Entry>();
+        yVals.add(new Entry(securityInfo.getData().getAggratingcalc().getTotalSellRatings(),0));
+        yVals.add(new Entry(securityInfo.getData().getAggratingcalc().getTotalHoldRatings(),1));
+        yVals.add(new Entry(securityInfo.getData().getAggratingcalc().getTotalBuyRatings(),2));
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("Sell");
+        xVals.add("Hold");
+        xVals.add("Buy");
+
+        PieDataSet dataSet = new PieDataSet(yVals, "Crowd Rating");
         dataSet.setSliceSpace(3f);
         dataSet.setSelectionShift(5f);
 
         dataSet.setColors(new int[] {R.color.price_red, R.color.company_name_gray, R.color.price_green}, context);
-
-        PieData data = new PieData(dataSet);
+        // instantiate pie data object now
+        PieData data = new PieData(xVals, dataSet);
         data.setValueFormatter(new PercentFormatter());
         data.setValueTextSize(11f);
         data.setValueTextColor(Color.WHITE);
         //data.setValueTypeface(mTfLight);
         pieChart.setData(data);
-
         // undo all highlights
         pieChart.highlightValues(null);
 
         pieChart.invalidate();
-*/
+
     }
 
 
-    private void setStrongPieChart(SecurityInfo securityInfo) {
-        pieChart2.setUsePercentValues(true);
-        pieChart2.setDescription("");
-        pieChart2.setExtraOffsets(5, 10, 5, 5);
-
-        pieChart2.setDragDecelerationFrictionCoef(0.95f);
-
-        pieChart2.setCenterText("Strong Buy/Sell");
-
-        pieChart2.setDrawHoleEnabled(true);
-        pieChart2.setHoleColor(Color.WHITE);
-
-        pieChart2.setTransparentCircleColor(Color.WHITE);
-        pieChart2.setTransparentCircleAlpha(110);
-
-        pieChart2.setHoleRadius(58f);
-        pieChart2.setTransparentCircleRadius(61f);
-
-        pieChart2.setDrawCenterText(true);
-
-        pieChart2.setRotationAngle(0);
-        // enable rotation of the chart by touch
-        pieChart2.setRotationEnabled(true);
-        pieChart2.setHighlightPerTapEnabled(true);
-
-        //set data
-        /*
-        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
-        entries.add(new PieEntry(securityInfo.getData().getAggratingcalc().getTotalStrongSellRatings(), "Sell"));
-        entries.add(new PieEntry(securityInfo.getData().getAggratingcalc().getTotalStrongBuyRatings(), "Buy"));
-        PieDataSet dataSet = new PieDataSet(entries, "Strong Buy/Sell");
-        dataSet.setSliceSpace(3f);
-        dataSet.setSelectionShift(5f);
-
-        dataSet.setColors(new int[] {R.color.price_red, R.color.price_green}, context);
-
-        PieData data = new PieData(dataSet);
-        data.setValueFormatter(new PercentFormatter());
-        data.setValueTextSize(11f);
-        data.setValueTextColor(Color.WHITE);
-        //data.setValueTypeface(mTfLight);
-        pieChart2.setData(data);
-
-        // undo all highlights
-        pieChart2.highlightValues(null);
-
-        pieChart2.invalidate();
-        */
-    }
 }
